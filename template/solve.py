e37013774252e40b8627b95fb7cc26af7cd0db15
Justin Stitt <jstitt007@gmail.com>
2023-11-30 20:51:06 -0800

    add template for python

---
+++ template/solve.py
@@ -0,0 +1,24 @@
+#! /home/jstitt/repos/advent-of-code-2023/.venv/bin/python3
+# Advent of Code 2023 - Day TODO
+import sys
+import itertools
+import functools
+from icecream import ic
+from collections import defaultdict
+
+
+class Soln:
+    def __init__(self, inp_file):
+        with open(inp_file, "r") as fd:
+            self.lines = [x.strip() for x in fd.readlines()]
+
+    def solve(self):
+        for line in self.lines:
+            ic(line)
+
+
+# fmt: off
+if __name__ == "__main__":
+    if len(sys.argv) > 2 and sys.argv[2] == "-d": ic.disable()
+    soln = Soln(sys.argv[1] + ".in" if len(sys.argv) > 1 else "small.in")
+    soln.solve()
