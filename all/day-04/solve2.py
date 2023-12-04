60d4d4b908139dbe78962a755e6dd3c3fa919160
Justin Stitt <jstitt007@gmail.com>
2023-12-03 21:42:21 -0800

    make faster

--- all/day-04/solve2.py
+++ all/day-04/solve2.py
@@ -7,6 +7,8 @@ import re
 from icecream import ic
 from collections import defaultdict
 
+import time
+
 
 class Soln:
     def __init__(self, inp_file):
@@ -16,16 +18,11 @@ class Soln:
             self.cols = len(self.lines[0])
 
     def solve(self):
-        total = 0
         card_freq = defaultdict(lambda: int())
-        card_val = defaultdict(lambda: int())
-        hits = defaultdict(lambda: int())
         for i, l in enumerate(self.lines):
             card_freq[i] += 1
-
-        idx = 0
-        while idx < len(self.lines) and card_freq[idx] > 0:
-            hits[idx] += 1
+        total = len(self.lines)
+        for idx, line in enumerate(self.lines):
             line = self.lines[idx]
             line = line.split(":")[1]
             line = line.split("|")
@@ -37,12 +34,9 @@ class Soln:
                     matches += 1
             # win the next 'matches' cards
             for y in range(idx + 1, idx + 1 + matches):
-                card_freq[y] += 1
-            # ic(card_freq)
-            card_freq[idx] -= 1
-            if card_freq[idx] == 0:
-                idx += 1
-        ic(sum(hits.values()))
+                card_freq[y] += 1 * card_freq[idx]
+                total += 1 * card_freq[idx]
+        print(total)
 
 
 # fmt: off
@@ -50,4 +44,8 @@ if __name__ == "__main__":
     if len(sys.argv) > 2 and sys.argv[2] == "-d": ic.disable()
     else: ic.configureOutput(includeContext=True)
     soln = Soln(sys.argv[1] + ".in" if len(sys.argv) > 1 else "small.in")
+
+    bfore = time.perf_counter()
     soln.solve()
+    after = time.perf_counter()
+    print('TIME: ', after-bfore)
